4e165ae47d736b91014fab6aeee1fee7bdd2eca8
Sorah Fukumori <her@sorah.jp>
2024-02-20 10:12:03 +0900

    tmp 1.29

--- versions.tf
+++ versions.tf
@@ -2,10 +2,10 @@
 # to generate the latest values for this
 locals {
   versions = {
-    k8s                = "1.25"
+    k8s                = "1.29"
     vpc_cni            = "v1.16.2-eksbuild.1"
-    kube_proxy         = "v1.25.9-eksbuild.1"
-    coredns            = "v1.9.3-eksbuild.9"
+    kube_proxy         = "v1.29.0-eksbuild.3"
+    coredns            = "v1.11.1-eksbuild.6"
     aws_ebs_csi_driver = "v1.27.0-eksbuild.1"
   }
 }
